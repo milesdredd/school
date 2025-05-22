45ce84987082615afc1d152e09046e12d1229299
milesdredd <87034849+milesdredd@users.noreply.github.com>
2025-05-22 15:57:40 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,15 +1,1 @@
-import pandas as pd
-a = input("1st category")
-b = input("2nd category")
-c = input("1st item of list" )
-d = input("2nd item of list" )
-e = input("3rd item of list" )
-f = input()
-g = input()
-h = input("of")
-data = {a:[c,d,e],b:[f,g,h]}
-ind=[1,2,3]
-
-table = pd.DataFrame(data,ind)
-
-print(table)
+https://github.com/Aftab48/Sem2-C-Lab/tree/main/assignment-4
